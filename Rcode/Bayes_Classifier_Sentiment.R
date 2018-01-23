d51811226b9094c81b0b2e7374bc39ea42a9a37a
Charles C.N. Wang <chaoneng.wang@gmail.com>
2018-01-23 18:05:01 +0800

    Update Bayes_Classifier_Sentiment.R

--- Rcode/Bayes_Classifier_Sentiment.R
+++ Rcode/Bayes_Classifier_Sentiment.R
@@ -1,7 +1,8 @@
 #Sentiment analysis with Naïve Bayes Classifier in R
-library(RTextTools)
+#載入套件
 library(e1071)
 
+#正面情感
 pos_tweets =  rbind(
   c('I love this car', 'positive'),
   c('This view is amazing', 'positive'),
@@ -10,6 +11,7 @@ pos_tweets =  rbind(
   c('He is my best friend', 'positive')
 )
 
+#負面情感
 neg_tweets = rbind(
   c('I do not like this car', 'negative'),
   c('This view is horrible', 'negative'),
@@ -18,6 +20,7 @@ neg_tweets = rbind(
   c('He is my enemy', 'negative')
 )
 
+#用來測試模型的數據
 test_tweets = rbind(
   c('feel happy this morning', 'positive'),
   c('larry friend', 'positive'),
@@ -28,19 +31,23 @@ test_tweets = rbind(
 
 tweets = rbind(pos_tweets, neg_tweets, test_tweets)
 
-# build dtm
+# 建構一個DocumentTermMatrix矩陣
 matrix= create_matrix(tweets[,1], language="english", 
                       removeStopwords=FALSE, removeNumbers=TRUE, 
                       stemWords=FALSE) 
 
+#因為我們使用create_matrix，所以他會是一個"DocumentTermMatrix"/"simple_triplet_matrix"，所以我們要轉換矩陣
 mat = as.matrix(matrix)
+
+#mat[1:10,]這一個指令是將正負面的情感作為訓練集，as.factor(tweets[1:10,2])是顯示正負情感分類
 classifier = naiveBayes(mat[1:10,], as.factor(tweets[1:10,2]) )
 
 
-# test the validity
+# 建利好模型開始預測，mat[11:15,]這一段指令就是，我們在前面預測要測試的數據集
 predicted = predict(classifier, mat[11:15,])
 predicted
 
+#測試結果跟準確度
 table(tweets[11:15, 2], predicted)
 recall_accuracy(tweets[11:15, 2], predicted)
 
